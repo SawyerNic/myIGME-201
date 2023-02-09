d2d9bfaef962d6b070ad44a71b152ff684c8708e
SawyerNic <111994367+SawyerNic@users.noreply.github.com>
2023-02-08 22:49:40 -0500

    Hello Guy

---
+++ PE8Q7/PE8Q7/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace PE8Q7
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //variables
+            string input;
+            char[] arr;
+            string output = "";
+
+            //get user input
+            Console.WriteLine("Enter a sentence");
+            input = Console.ReadLine();
+
+            arr = input.ToCharArray();
+
+            Console.WriteLine(arr.Length);
+
+            foreach(char i in arr)
+            {
+                output = i + output;
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
