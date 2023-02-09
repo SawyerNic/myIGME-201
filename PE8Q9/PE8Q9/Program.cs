035e9b82028ef6412b1a256f0bd4848c9d8edfc8
SawyerNic <111994367+SawyerNic@users.noreply.github.com>
2023-02-08 23:11:15 -0500

    PE8

---
+++ PE8Q9/PE8Q9/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace PE8
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //variables
+            string input;
+            string output = "";
+            string[] arr;
+
+            //get input
+            input = Console.ReadLine();
+
+            arr = input.Split(" ");
+
+            foreach(string i in arr)
+            {
+                output = output + " \"" + i + "\"";
+            }
+
+            Console.WriteLine(output);
+
+        }
+    }
+}
