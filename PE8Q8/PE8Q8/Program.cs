10e702e980f27b0d97bb0d99dc93e4056840595d
SawyerNic <111994367+SawyerNic@users.noreply.github.com>
2023-02-08 23:01:26 -0500

    Yes No

---
+++ PE8Q8/PE8Q8/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace PE8Q8
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //varibles
+            string input;
+            string output = "";
+            string[] arr;
+
+            //prompt user
+            Console.WriteLine("Enter a sentance");
+
+            input = Console.ReadLine();
+
+            //make same case
+            input = input.ToLower();
+
+            arr = input.Split(" ");
+
+            foreach(string i in arr)
+            {
+                if (i == "no")
+                {
+                    output = output + "yes ";
+                }
+                else
+                {
+                    output = output + i + " ";
+                }
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
