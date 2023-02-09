1da91b7367f532b3318d21e3f14368fdc8fca4ae
SawyerNic <111994367+SawyerNic@users.noreply.github.com>
2023-02-08 21:55:29 -0500

    PE8

--- PE7/PE7/Program.cs
+++ PE7/PE7/Program.cs
@@ -1,3 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace MadLibs
+{
+
+    class Program
+    {
+        enum color : byte
+        {
+            red = 1,
+            orange = 2,
+            yellow = 3,
+            green = 4,
+            blue = 5,
+            indigo = 6,
+            violet = 7,
+            white = 8,
+            black = 9
+        }
+
+        private static void Main(string[] Args)
+        {
+
+            //StreamReader template = new StreamReader("/Users/sawyernicastro/Desktop/myIGME-201/PE7/PE7/MadLibsTemplate.txt");
+
+            //Console.WriteLine(template);
+
+            double[] precipitation = new double[4] { .45, 2.78, .04, 1.22 };
+
+
+
+        }
+
+        
+    }
+}
---
+++ PE8-Question5/PE8-Question5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace PE8
+{
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            /*5.	Given the formula z = 3y2 + 2x - 1 
+        * write a console application to calculate 
+        * z for the following ranges of x and y:
+        */
+
+            //variables
+            int xI;
+            int yI;
+            int zI = 0;
+            double x = -1;
+            double y = 1;
+            double z = 0;
+            double[,,] arr = new double[20,30,600];
+
+            //outer loop
+            for (xI = 0; x <= 1; xI += 1)
+            {
+                
+                //inner loop
+                for(yI = 0; y <= 4; yI += 1)
+                {
+                    z = (3*y*y) + (2*x) - 1;
+                    arr[xI, yI, zI] = z;
+                    y += .1;
+                    zI += 1;
+                    Console.WriteLine(arr[xI, yI, zI]);
+                    Console.WriteLine(x + " " + y + " " + z);
+                }
+                x += .1;
+
+            }
+        }
+    }
+}
