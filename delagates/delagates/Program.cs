d74c8270e4681357c76fc660dc0593002df25dde
SawyerNic <111994367+SawyerNic@users.noreply.github.com>
2023-02-14 19:35:17 -0500

    delegates

--- delagates/delagates/Program.cs
+++ delagates/delagates/Program.cs
@@ -1,3 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace Delagates
+{
+    class Program
+    {
+        //definition of delegate function
+        delegate string ReadInput();
+
+        /// delegate steps
+        /// 1. define the delegate method data type based on the method signature
+        ///         delegate double MathFunction(double n1, double n2);
+        /// 2. declare the delegate method variable
+        ///         MathFunction processDivMult;
+        /// 3. point the variable to the method that it should call
+        ///         processDivMult = new MathFunction(Multiply);
+        /// 4. call the delegate method / add the delegate method to the object's event handler
+        ///         nAnswer = processDivMult(n1, n2);
+        ///         timer.Elapsed += TimesUp;
+        public static void Main()
+        {
+
+            ReadInput readInput;
+            readInput = new ReadInput(Console.ReadLine);
+
+            string userInput = readInput();
+
+
+        }
+    }
+}
