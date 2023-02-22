e14916d7bc44fb09c5ca4ca6f837e76c6c3c1301
SawyerNic <111994367+SawyerNic@users.noreply.github.com>
2023-02-21 19:17:24 -0500

    umlReplica

---
+++ umlReplica/umlReplica/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Replication
+{
+    class Program
+    {
+        public class HotDrink
+        {
+            string milk;
+            string sugar;
+
+            public void Drink()
+            {
+
+            }
+
+            public void AddMilk()
+            {
+
+            }
+
+            public void AddSugar()
+            {
+
+            }
+
+
+        }
+
+        public static void consume(HotDrink drink)
+        {
+            drink.Drink();
+            drink.AddMilk();
+            drink.AddSugar();
+        }
+    }
+}
