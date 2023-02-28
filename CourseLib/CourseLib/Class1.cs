a5a39255c07c16ab39d49851de556396242d6f23
SawyerNic <111994367+SawyerNic@users.noreply.github.com>
2023-02-27 23:53:35 -0500

    push

--- CourseLib/CourseLib/Class1.cs
+++ CourseLib/CourseLib/Class1.cs
@@ -1,6 +1,30 @@
 ﻿namespace CourseLib;
-public class Class1
+
+public class Schedule
 {
+    public DateTime startTime;
+    public DateTime endTime;
+    public List<DayOfWeek> daysOfWeek;
+}
+
+public class Courses 
+{
+    public SortedList<string, Course> sortedList;
+    public string this[string key]
+    {
+        get { return sortedList[key]; }
+        set { sortedList[key] = value; }
+    }
+    
 
 }
 
+public class Course : IComparable<Course>
+{
+    public string courseCode;
+    public string description;
+    public string teacherEmail;
+    public Schedule Schedule;
+
+
+}
