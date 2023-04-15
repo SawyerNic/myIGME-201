7e59846bcf63854816dad8974c651357199fdb57
SawyerNic <111994367+SawyerNic@users.noreply.github.com>
2023-04-14 20:46:52 -0400

    pe20

---
+++ PE20Dom/PE20Dom/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static System.Net.Mime.MediaTypeNames;
+
+try
+{
+    Microsoft.Win32.RegistryKey key = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(
+        @"SOFTWARE\\WOW6432Node\\Microsoft\\Internet Explorer\\MAIN\\FeatureControl\\FEATURE_BROWSER_EMULATION",
+        true);
+    key.SetValue(Application.ExecutablePath.Replace(Application.StartupPath + "\\", ""), 12001, Microsoft.Win32.RegistryValueKind.DWord);
+    key.Close();
+}
+catch
+{
+
+}
