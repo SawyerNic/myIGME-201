7767ba6c8777ca7b77c6f0fd856b1b83bdd29b07
SawyerNic <111994367+SawyerNic@users.noreply.github.com>
2023-04-23 23:35:18 -0400

    typing test

--- PE20Dom/PE20Dom/Program.cs
+++ PE20Dom/PE20Dom/Program.cs
@@ -1,15 +1,2 @@
-﻿using System;
-using static System.Net.Mime.MediaTypeNames;
-
-try
-{
-    Microsoft.Win32.RegistryKey key = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(
-        @"SOFTWARE\\WOW6432Node\\Microsoft\\Internet Explorer\\MAIN\\FeatureControl\\FEATURE_BROWSER_EMULATION",
-        true);
-    key.SetValue(Application.ExecutablePath.Replace(Application.StartupPath + "\\", ""), 12001, Microsoft.Win32.RegistryValueKind.DWord);
-    key.Close();
-}
-catch
-{
-
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
